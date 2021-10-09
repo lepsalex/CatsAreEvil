f197d7c4354e11313b1c8376ae8683591aaf52bf
Alex <lepsalex@gmail.com>
2021-10-09 13:49:03 -0400

    basic enemy damage and destroy

---
+++ Assets/Enemy/Enemy.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health;
+
+    public void TakeDamage(int damageAmount)
+    {
+        health -= damageAmount;
+
+        if (health <= 0) Destroy(gameObject);
+    }
+}
--- Assets/Weapon/Projectile.cs
+++ Assets/Weapon/Projectile.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    public int damage;
     public float speed;
     public float lifetime;
 
@@ -23,5 +25,14 @@ public class Projectile : MonoBehaviour
     {
         Instantiate(explosion, transform.position, Quaternion.identity);
         Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag.Equals("Enemy"))
+        {
+            other.GetComponent<Enemy>().TakeDamage(damage);
+            DestroyProjectile();
+        }
     }
 }
